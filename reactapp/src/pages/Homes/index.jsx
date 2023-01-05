969b7a8c25b20b9131c39f84237d7ebb4d43a017
Geraldo Pires <geraldopireslacerda771@gmail.com>
2023-01-05 01:48:52 -0300

    Aprendendo sobre componentes

--- reactapp/src/pages/Homes/index.jsx
+++ reactapp/src/pages/Homes/index.jsx
@@ -1,6 +1,8 @@
 //import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
-import './styles.css'
+import './styles.css';
+
+import { Card } from '../../componentes/Card';
 
 export function Home() {
   //const [count, setCount] = useState(0)
@@ -10,6 +12,13 @@ export function Home() {
     <h1>Lista de Presença</h1>
     <input type="text" placeholder="Digite o nome..." />
     <button type="button">Adicionar</button>
+
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
     </div>
   )
 }
