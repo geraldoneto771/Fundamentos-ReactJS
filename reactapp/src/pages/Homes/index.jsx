5edd2646224442a8dc727b0ed8008ff31a06e913
Geraldo Pires <geraldopireslacerda771@gmail.com>
2023-01-06 00:26:15 -0300

    Aprendendo sobre Imutabilidade

--- reactapp/src/pages/Homes/index.jsx
+++ reactapp/src/pages/Homes/index.jsx
@@ -6,26 +6,64 @@ import { Card } from '../../componentes/Card';
 
 export function Home() {
 
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }
+
+  let  today = new Date();
+    
+  // estado e função para armazenar valor digitado pelo usuário
   const [tarefasName, setTarefasName] = useState();
 
+  // estado e função para armazenar as tarefas da lista de tarefas
+  const [tarefas, setTarefas] = useState([]);
+
+  // criando um novo objeto
+  function handleAddTarefa(){
+      const newTarefa = {
+        name: tarefasName,
+        time: new Date().toLocaleTimeString("pt-br", {
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit',
+        })
+      };
+
+      // adicionando o objeto no estado tarefas, o prevState é uma variavel auxiliar que pode ter qualquer nome, no qual vai salvar o conteudo anterior
+      setTarefas(prevState => [...prevState, newTarefa])
+      document.getElementById('inputName').value = '';
+      document.getElementById('inputName').focus();
+  }
 
   return (
     <div className="container">
-    <h1>Lista de Tarefas</h1>
-    <p>{tarefasName}</p>
-    <input 
+    <h1>Meu Planner 2023</h1>
+    
+    <p>{today.toLocaleString("pt-BR", options)}</p>
+    <input id='inputName'
       type="text" 
      placeholder="Digite uma tarefa..." 
      onChange={e => setTarefasName(e.target.value)}
     />
     
-    <button type="button">Adicionar</button>
+    <button type="button" onClick={handleAddTarefa}>
+      Adicionar</button>
 
-    <Card name="Devocional" time="1h30"/>
-    <Card name="Estudar Frontend" time="2h00"/>
-    <Card name="Estudar Administração" time="1h00"/>
-    <Card name='Estudar Psicologia' time='1h00'/>
+    {
+      tarefas.map(tarefa => <Card name={tarefa.name} time={tarefa.time}/>)
+      
+    }
+    
     </div>
   )
 }
 
+// IDEIAS PARA IMPLEMENTAR
+/*
+ - Adicionar checkbox
+ - Adicionar botão de excluir e/ou editar
+ - Adicionar botão para ordenar em lista ou blocos/grades
+ - Alterar o design para definir dias, mes, semanas, observações etc.
+ */
